90cfa12b79a7e9f895564c143c4af668cd2278a4
BraedonMyatt <braedonmyatt@gmail.com>
2026-01-25 23:46:25 -0400

    firebase realtime database configured, attached to the android studio project

--- app/src/main/java/com/example/csci3130groupproject/MainActivity.java
+++ app/src/main/java/com/example/csci3130groupproject/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.csci3130groupproject;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase database;
+    DatabaseReference dbref;
+    String DB_URL = "https://csci3130groupproject-c46e6-default-rtdb.firebaseio.com/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        database = FirebaseDatabase.getInstance(DB_URL);
+        //dbref = database.getReference("Table");
     }
 }
