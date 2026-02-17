f6d59247b5ff0793e5e934394ea0a417167769d4
BraedonMyatt <braedonmyatt@gmail.com>
2026-02-17 18:04:05 -0400

    refactored signup function and added redirection to the login page on success

--- app/src/main/java/com/example/csci3130groupproject/FirebaseDB.java
+++ app/src/main/java/com/example/csci3130groupproject/FirebaseDB.java
@@ -6,9 +6,12 @@ import android.util.Log;
 import android.widget.Toast;
 import android.content.Intent;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FirebaseDB {
     FirebaseDatabase db;
@@ -34,32 +37,34 @@ public class FirebaseDB {
         return auth.getCurrentUser();
     }
 
-    public void addUser(String name, String email, String password, String role, Context context){
-        // create auth user in firebase, if successful store in db, on fail print toast
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener( task -> {
-            if (task.isSuccessful()) {
-                Log.d("Successful", "auth completed");
+    public Task<Void> addUser(String name, String email, String password, String role, Context context){
+        // check if successful
+        // create auth user in firebase, if successful store in db, on fail return boolean
+        return auth.createUserWithEmailAndPassword(email, password).continueWithTask( task -> {
 
-                // get User id for current user
-                FirebaseUser user = auth.getCurrentUser();
-                String uid = user.getUid();
-
-                // create user for db storage
-                User you = new User(name, email, role);
-
-                // store in db under users->(user id key) and print error/success
-                db.getReference().child("users").child(uid).setValue(you).addOnCompleteListener( write ->{
-                    if (write.isSuccessful()) {
-                        Toast.makeText(context, "Account Created", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(context, "DB write failed", Toast.LENGTH_SHORT).show();
-                        Log.e("DB write", "write failed", task.getException());
-                    }
-                });
-            } else {
-                Toast.makeText(context, "Account authentification failed, already exists", Toast.LENGTH_SHORT).show();
+            if(!task.isSuccessful()) {
                 Log.e("AUTH", "createUser failed", task.getException());
+                throw task.getException();
             }
+
+            Log.d("Successful", "auth completed");
+
+            // get User id for current user
+            FirebaseUser user = auth.getCurrentUser();
+            String uid = user.getUid();
+
+            // create user for db storage
+            User you = new User(name, email, role);
+
+            // store in db under users->(user id key) and print error/success
+            return db.getReference().child("users").child(uid).setValue(you).addOnCompleteListener( write ->{
+                if (write.isSuccessful()) {
+                    Toast.makeText(context, "Account Created", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "DB write failed", Toast.LENGTH_SHORT).show();
+                    Log.e("DB write", "write failed", task.getException());
+                }
+            });
         });
     }
 
