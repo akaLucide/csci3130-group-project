80dd89aab007ae00adba941ee3247b359e7fb8c1
Mitchell Haydar <mt639784@dal.ca>
2026-03-11 14:01:50 -0300

    Add Google Maps integration

--- app/src/main/java/com/example/csci3130groupproject/ui/MapBasedJobViewingActivity.java
+++ app/src/main/java/com/example/csci3130groupproject/ui/MapBasedJobViewingActivity.java
@@ -1,4 +1,44 @@
 package com.example.csci3130groupproject.ui;
 
-public class MapBasedJobViewingActivity {
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.csci3130groupproject.R;
+import com.example.csci3130groupproject.databinding.ActivityMapBasedJobViewingBinding;
+
+public class MapBasedJobViewingActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapBasedJobViewingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapBasedJobViewingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng halifax = new LatLng(44.6488, -63.5752);
+        mMap.addMarker(new MarkerOptions().position(halifax).title("Halifax"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(halifax, 11f));
+    }
 }
